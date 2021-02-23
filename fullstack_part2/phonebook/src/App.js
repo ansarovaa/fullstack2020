65b71b8e636837a49d85fc855e31f2ef7f51852f
Anar Ansarova <Anar_Ansarova@epam.com>
2021-02-23 12:37:18 +0600

    task 2.11

--- fullstack_part2/phonebook/src/App.js
+++ fullstack_part2/phonebook/src/App.js
@@ -1,35 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect } from 'react'
+import axios from 'axios'
 import People from './components/People'
 import AddPeople from './components/AddPeople'
 import FindPeople from './components/FindPeople'
 
 const App = () => {
     const [persons,
-        setPersons] = useState([
-        {
-            name: 'Arto Hellas',
-            number: '040-123456',
-            id: 1
-        }, {
-            name: 'Ada Lovelace',
-            number: '39-44-5323523',
-            id: 2
-        }, {
-            name: 'Dan Abramov',
-            number: '12-43-234345',
-            id: 3
-        }, {
-            name: 'Mary Poppendieck',
-            number: '39-23-6423122',
-            id: 4
-        }
-    ]) //initial state of names in phone book
+        setPersons] = useState([]) //initial state of names in phone book
     const [newName,
         setNewName] = useState('')
     const [newNumber,
         setNewNumber] = useState('')
     const [findName,
         findNameInList] = useState('')
+
+        useEffect(() => {
+            console.log('effect')
+            axios
+              .get('http://localhost:3001/persons')
+              .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+              })
+          }, [])
+        
+          console.log('render', persons.length, 'persons')
 
     const addNameNumber = (event) => { //function to add new name into phonebook
         event.preventDefault()
