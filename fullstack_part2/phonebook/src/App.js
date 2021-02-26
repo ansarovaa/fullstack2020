5a270ed35b8aa00aa7d690f3f3dca41076ee5fda
Anar Ansarova <Anar_Ansarova@epam.com>
2021-02-26 10:42:04 +0600

    task 2.19

--- fullstack_part2/phonebook/src/App.js
+++ fullstack_part2/phonebook/src/App.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import personService from './services/person'
+import './index.css'
 import People from './components/People'
 import AddPeople from './components/AddPeople'
 import FindPeople from './components/FindPeople'
+import Notification from './components/Notification'
 
 
 const App = () => {
@@ -51,8 +53,10 @@ const App = () => {
                         setPersons(persons.map(person => person.id !== returned.id
                             ? person
                             : returned))
+                        setMessage(
+                                `Updated number for '${newName}'`
+                              )
                     })
-                    .then(setMessage(`'${newObject.name}' has been updated.`))
                     .catch(error => {
                         setError(`${error}`)
                     })
@@ -67,6 +71,9 @@ const App = () => {
                 .create(newObject)
                 .then(returnedName => {
                     setPersons(persons.concat(returnedName))
+                    setMessage(
+                        `Added '${newName}'`
+                      )
                 })
             setNewName('')
             setNewNumber('')
@@ -74,55 +81,55 @@ const App = () => {
         }
     }
 
-        const deletePersonFrom = (id) => {
-            const result = window.confirm("Confirm delete")
-            try {
-                if (result) {
-                    personService
-                        .deletePerson(id)
-                        .then(setPersons(persons.filter(person => person.id !== id)))
-                }
-
-            } catch (error) {
-                setError('Error occured')
-                setTimeout(() => {
-                    setMessage(null)
-                }, 3000)
+    const deletePersonFrom = (id) => {
+        const result = window.confirm("Confirm delete")
+        try {
+            if (result) {
+                personService
+                    .deletePerson(id)
+                    .then(setPersons(persons.filter(person => person.id !== id)))
             }
+
+        } catch (error) {
+            setError('Error occured')
+            setTimeout(() => {
+                setMessage(null)
+            }, 3000)
         }
-
-        const handleNameChange = (event) => {
-            setNewName(event.target.value)
-        }
-
-        const handleNumberChange = (event) => {
-            setNewNumber(event.target.value)
-        }
-
-        const handleNameFind = (event) => {
-            findNameInList(event.target.value)
-        }
-
-        return (
-            <div>
-                <h1>Phonebook</h1>
-                <FindPeople findName={findName} handleNameFind={handleNameFind}/>
-                <h2>Add a new</h2>
-                <AddPeople
-                    addNameNumber={addNameNumber}
-                    newName={newName}
-                    handleNameChange={handleNameChange}
-                    handleNumberChange={handleNumberChange}
-                    newNumber={newNumber}/>
-                <h2>Numbers</h2>
-
-                <People
-                    persons={persons}
-                    findName={findName}
-                    deletePersonFrom={deletePersonFrom}/>
-            </div>
-        )
     }
 
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
 
-    export default App
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    const handleNameFind = (event) => {
+        findNameInList(event.target.value)
+    }
+
+    return (
+        <div>
+            <h1>Phonebook</h1>
+            <Notification message = {message}/>
+            <FindPeople findName={findName} handleNameFind={handleNameFind}/>
+            <h2>Add a new</h2>
+            <AddPeople
+                addNameNumber={addNameNumber}
+                newName={newName}
+                handleNameChange={handleNameChange}
+                handleNumberChange={handleNumberChange}
+                newNumber={newNumber}/>
+            <h2>Numbers</h2>
+
+            <People
+                persons={persons}
+                findName={findName}
+                deletePersonFrom={deletePersonFrom}/>
+        </div>
+    )
+}
+
+export default App
